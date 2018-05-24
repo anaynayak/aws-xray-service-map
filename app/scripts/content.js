c3ebaed458a4f140e3d3496d9c83958cde1c98e2
Anay Nayak <anay.nayak@vuclip.com>
2018-05-24 23:22:12 +0530

    Switch to chroma.js

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -13,41 +13,29 @@ function colorize(highlightNodes, hideNodes) {
         }
     });
 
-    function perc2color(perc) {
-        var r, g, b = 0;
-        if (perc < 50) {
-            r = 255;
-            g = Math.round(5.1 * perc);
-        }
-        else {
-            g = 255;
-            r = Math.round(510 - 5.10 * perc);
-        }
-        var h = r * 0x10000 + g * 0x100 + b * 0x1;
-        return '#' + ('000000' + h.toString(16)).slice(-6);
-    }
-
     var map = new Map();
     $(".req-per-min").each(function () {
         var reqs = $(this).text().replace(/t\/min/g, '');
-        if (parseInt(reqs)) {
+        if (parseFloat(reqs)) {
             var requests = parseFloat(reqs) * (reqs.includes('k') ? 1000 : 1);
             map.set($(this), Math.log(requests));
         }
     });
 
-    var max = Array.from(map.values()).reduce(function (a, b) {
-        return Math.max(a, b);
-    })
+    var perc2color = chroma.scale(['lightgreen', 'yellow', 'red'])
+        .domain([1, 0])
+        .padding(0.15)
+        .classes(chroma.limits(Array.from(map.values()), 'e', 10));
+
     map.forEach(function (reqs, elem) {
-        var color = perc2color(100 - (reqs * 100 / max))
+        var color = perc2color(reqs).hex();
         elem.parent().children('circle').css('fill', color);
     });
 }
 chrome.runtime.onMessage.addListener(function callback(message, sender) {
     chrome.storage.sync.get({
-        "highlightNodes": '(prd|Prod)',
-        "hideNodes": "(dev|Dev|e2e|E2E)"
+        highlightNodes: '(prd|Prod)',
+        hideNodes: "(dev|Dev|e2e|E2E)"
     }, function (data) {
         colorize(data.highlightNodes, data.hideNodes);
     });
