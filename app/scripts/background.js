82d1068b3ec5c58bf4dd73554fddf7bbe15ec0c9
Anay Nayak <anay.nayak@vuclip.com>
2018-05-29 18:46:06 +0530

    Enable extension only on AWS X-ray tabs

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function (activeTab) {
+chrome.pageAction.onClicked.addListener(function (activeTab) {
     chrome.tabs.sendMessage(activeTab.id, { colorize: true });
 });
 
@@ -6,4 +6,19 @@ chrome.commands.onCommand.addListener(function (command) {
     chrome.tabs.query({ active: true, currentWindow: true, windowType: 'normal' }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { colorize: true })
     })
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlMatches: '.*\.aws\.amazon\.com\/xray\/home*' },
+                    })
+                ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
+    });
 });
